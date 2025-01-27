c6b96a942f59c3715a3abbcc8d6fca459d444221
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-01-27 10:51:49 +0100

    see car infomation using ngfor and @for

--- src/app/components/for-latest/for-latest.component.ts
+++ src/app/components/for-latest/for-latest.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { cars } from '../../data/mocks/mockData';
 
 @Component({
   selector: 'app-components-for-latest',
   standalone: false,
-  
+
   templateUrl: './for-latest.component.html',
-  styleUrl: './for-latest.component.css'
+  styleUrl: './for-latest.component.css',
 })
 export class ForLatestComponent {
-
+  public title: string = 'For Latest';
+  public cars: any[] = cars;
 }
--- src/app/components/for-old/for-old.component.ts
+++ src/app/components/for-old/for-old.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { cars } from '../../data/mocks/mockData';
 
 @Component({
   selector: 'app-components-for-old',
   standalone: false,
-  
+
   templateUrl: './for-old.component.html',
-  styleUrl: './for-old.component.css'
+  styleUrl: './for-old.component.css',
 })
 export class ForOldComponent {
-
+  public title: string = 'For Old';
+  public cars: any[] = cars;
 }
